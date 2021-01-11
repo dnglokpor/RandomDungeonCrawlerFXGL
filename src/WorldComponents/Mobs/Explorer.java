6215b48e79b8f7b06e5c93caea4e523c339d090b
dnglokpor <delwys.glokpor@principia.edu>
2021-01-11 02:04:38 -0600

    day9 - finalized explorer object.

--- src/WorldComponents/Mobs/Explorer.java
+++ src/WorldComponents/Mobs/Explorer.java
@@ -5,7 +5,9 @@ import WorldComponents.Basics.Jobs.Job;
 import WorldComponents.Basics.StorageKits.Bag;
 import WorldComponents.Basics.StorageKits.Wallet;
 import WorldComponents.Basics.Unit;
+import WorldComponents.Basics.Leveling.ExplorerRank;
 import Customs.Exceptions.WrongSizeStatsArrayException;
+import Customs.Exceptions.NullActionArgumentException;
 
 /** Explorer object:
  * explorers are the core of this game as they are the people who challenge the dungeon in their quest for
@@ -20,15 +22,69 @@ public class Explorer extends Unit{
     Job job;
     Bag bag;
     Wallet wallet;
-    // TODO create explorer Card object
+    ExplorerRank rank;
 
-    public Explorer(String name, int initLevel, int[] stats, Job job) throws WrongSizeStatsArrayException {
+    /**
+     * expects a name, an initial level, a list of init values for all stats and a Job object.
+     * @param name the name of the explorer.
+     * @param initLevel the level to start the explorer's experience at.
+     * @param stats array of initial stats for all base units' stats.
+     * @param job a Job object set to the job the explorer follows.
+     * @throws WrongSizeStatsArrayException when the stat list is too short.
+     * @throws NullActionArgumentException when the job doesn't define a basic action a.k.a action at lvl 1.
+     */
+    public Explorer(String name, int initLevel, int[] stats, Job job) throws Exception {
         // init unit properties
         super(name, initLevel, stats);
         this.body = new Body();
         this.job = job;
         this.bag = new Bag();
         this.wallet = new Wallet();
-        // TODO init explorer Card
+        this.rank = new ExplorerRank(0);
+
+        // set basic action to match the job's
+        this.setJobBasic();
     }
+
+    // getters
+    /**
+     * @return the explorer's body.
+     */
+    public Body body(){ return this.body; }
+    /**
+     * @return the explorer's job.
+     */
+    public Job job(){ return this.job; }
+    /**
+     * @return the explorer's bag.
+     */
+    public Bag bag(){ return this.bag; }
+    /**
+     * @return the explorer's wallet.
+     */
+    public Wallet wallet(){ return this.wallet; }
+    /**
+     * @return the explorer's job.
+     */
+    public ExplorerRank rank(){ return this.rank; }
+
+    // setters
+    /**
+     * sets the basic action from the current job as the explorer's basic action.
+     */
+    public void setJobBasic() throws NullActionArgumentException {
+        this.abilities.setBasic(this.job.learnedAt(1));
+    }
+    /**
+     * call when wanting to change the job of an explorer altogether.
+     * @param newJob the Job object to switch to.
+     * @return the old Job.
+     */
+    public Job changeJob(Job newJob) throws NullActionArgumentException {
+        Job old = this.job; // save old reference
+        this.job = newJob; // point to new reference
+        this.setJobBasic(); // update basic ability
+        return old;
+    }
+
 }
