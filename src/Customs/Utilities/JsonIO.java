fb2a2eda4e4abaf19e45edd5949e11958bb5c02e
dnglokpor <delwys.glokpor@principia.edu>
2021-01-11 02:08:33 -0600

    day9 - added methods to create Item from JSON.

--- src/Customs/Utilities/JsonIO.java
+++ src/Customs/Utilities/JsonIO.java
@@ -4,14 +4,15 @@ import java.io.FileReader;
 import java.util.Iterator;
 import java.util.Map;
 
-import Customs.Exceptions.UndefinedKeyException;
-import WorldComponents.Basics.Actions;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 import WorldComponents.Basics.Actions.*;
 import WorldComponents.Basics.Elements.*;
+import WorldComponents.Basics.Collectibles.Item;
+import WorldComponents.Mobs.Hostiles.Monster;
+import Customs.Exceptions.UndefinedKeyException;
 
 /** JsonIO package:
  * this package contains custom functions that allow parsing game data from .json files. this is done using
@@ -22,17 +23,21 @@ import WorldComponents.Basics.Elements.*;
 public class JsonIO {
     // datafiles paths
     final static String ACTION_PATH = "./resources/JSON/Actions/";
+    final static String ITEM_PATH = "./resources/JSON/Items/";
 
     /**
      * loads the data for the action specified by the name from the ACTION_PATH directory and makes it an Action.
      * @param actionName the action to look for.
      * @return built Action object.
-     * @throws Exception FileNotFoundException, IOException, ParseException, UndefinedKeyException
+     * @throws java.io.FileNotFoundException when the specified file couldn't be found
+     * @throws java.io.IOException when reading the specified file failed
+     * @throws org.json.simple.parser.ParseException when the json is empty or not valid.
+     * @throws UndefinedKeyException when the json contains unexpected keys.
      */
     public static Action loadAction(String actionName) throws Exception {
-        String filename = ACTION_PATH + actionName + ".json"; // turn name into json filename
+        String filepath = ACTION_PATH + actionName + ".json"; // turn name into json filename
         // load data into memory
-        Object obj = new JSONParser().parse(new FileReader(filename));
+        Object obj = new JSONParser().parse(new FileReader(filepath));
         // cast as JSONObject
         JSONObject action = (JSONObject) obj;
 
@@ -71,7 +76,7 @@ public class JsonIO {
         int cooldown = (int) action.get("cooldown");
         int duration = (int) action.get("duration");
         String[] stargets = null; // placeholder
-        if(!(action.get("stats") == null)) { // only build stats list if there is one
+        if(!(type.equals("agress"))) { // only build stats list not Agress action.
             JSONArray ja = (JSONArray) action.get("stats");
             stargets = new String[ja.size()];
             int i = 0; // index for array
@@ -95,4 +100,37 @@ public class JsonIO {
         // return it
         return generated;
     }
+
+    /**
+     * loads the data for the item specified by the name from the ITEM_PATH directory and makes it an Item.
+     * @param itemName the item to look for.
+     * @return built Item object.
+     * @throws java.io.FileNotFoundException when the specified file couldn't be found
+     * @throws java.io.IOException when reading the specified file failed
+     * @throws org.json.simple.parser.ParseException when the json is empty or not valid.
+     */
+    public static Item loadItem(String itemName) throws Exception {
+        String filepath = ITEM_PATH + itemName + ".json"; // turn name into json filename
+        // load data into memory
+        Object obj = new JSONParser().parse(new FileReader(filepath));
+        // cast as JSONObject
+        JSONObject item = (JSONObject) obj;
+        // create Item and return it
+        return new Item((String) item.get("name"), (String) item.get("description"),
+            (int) item.get("value"));
+    }
+
+    /**
+     * loads the data for the monster specified by the name from the MONSTER_PATH directory and makes it
+     * a Monster or a Boss.
+     * @param monsterName the item to look for.
+     * @return built Monster object or Boss object.
+     * @throws java.io.FileNotFoundException when the specified file couldn't be found
+     * @throws java.io.IOException when reading the specified file failed
+     * @throws org.json.simple.parser.ParseException when the json is empty or not valid.
+     */
+    //TODO public static Monster loadMonster(String itemName) throws Exception {
+
+
+    //}
 }
