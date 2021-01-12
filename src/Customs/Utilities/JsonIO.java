8f904ce4d5af1fb14baec68b28b2025272a48865
dnglokpor <delwys.glokpor@principia.edu>
2021-01-12 00:15:58 -0600

    day10 - added monster construction from JSON.

--- src/Customs/Utilities/JsonIO.java
+++ src/Customs/Utilities/JsonIO.java
@@ -12,6 +12,7 @@ import WorldComponents.Basics.Actions.*;
 import WorldComponents.Basics.Elements.*;
 import WorldComponents.Basics.Collectibles.Item;
 import WorldComponents.Mobs.Hostiles.Monster;
+import WorldComponents.Mobs.Hostiles.Boss;
 import Customs.Exceptions.UndefinedKeyException;
 
 /** JsonIO package:
@@ -24,6 +25,7 @@ public class JsonIO {
     // datafiles paths
     final static String ACTION_PATH = "./resources/JSON/Actions/";
     final static String ITEM_PATH = "./resources/JSON/Items/";
+    final static String HOSTILES_PATH = "./resources/JSON/Hostiles/";
 
     /**
      * loads the data for the action specified by the name from the ACTION_PATH directory and makes it an Action.
@@ -40,9 +42,6 @@ public class JsonIO {
         Object obj = new JSONParser().parse(new FileReader(filepath));
         // cast as JSONObject
         JSONObject action = (JSONObject) obj;
-
-        // make reference to returned object
-        Action generated;
         // recover action type for calling right constructor
         String type = (String) action.get("type");
         // recover action data
@@ -77,17 +76,11 @@ public class JsonIO {
         int duration = (int) action.get("duration");
         String[] stargets = null; // placeholder
         if(!(type.equals("agress"))) { // only build stats list not Agress action.
-            JSONArray ja = (JSONArray) action.get("stats");
-            stargets = new String[ja.size()];
-            int i = 0; // index for array
-            for (Object o : ja) {
-                stargets[i] = (String) o;
-                i++; // update array position
-            }
+            stargets = Helpers.jsonArrayToStringArray((JSONArray) action.get("stats"));
         }
         String description = (String) action.get("description");
-        // build action based on its type
-        generated = switch (type){
+        // build action based on its type return it
+        return switch (type){
             case "agress" -> new Agress(name, attribute, category, target, multiplier, accuracy, cooldown,
                 duration, description);
             case "buff" -> new Buff(name, attribute, category, target, multiplier, accuracy, cooldown,
@@ -96,14 +89,11 @@ public class JsonIO {
                     duration, stargets, description);
             default -> {throw new UndefinedKeyException(type + "is not a valid Action type.");}
         };
-
-        // return it
-        return generated;
     }
 
     /**
      * loads the data for the item specified by the name from the ITEM_PATH directory and makes it an Item.
-     * @param itemName the item to look for.
+     * @param itemName the name of the item to look for.
      * @return built Item object.
      * @throws java.io.FileNotFoundException when the specified file couldn't be found
      * @throws java.io.IOException when reading the specified file failed
@@ -123,14 +113,52 @@ public class JsonIO {
     /**
      * loads the data for the monster specified by the name from the MONSTER_PATH directory and makes it
      * a Monster or a Boss.
-     * @param monsterName the item to look for.
+     * @param hostileName the name of the monster to look for.
      * @return built Monster object or Boss object.
      * @throws java.io.FileNotFoundException when the specified file couldn't be found
      * @throws java.io.IOException when reading the specified file failed
      * @throws org.json.simple.parser.ParseException when the json is empty or not valid.
      */
-    //TODO public static Monster loadMonster(String itemName) throws Exception {
-
-
-    //}
+    public static Monster loadHostile(String hostileName) throws Exception {
+        String filepath = HOSTILES_PATH + hostileName + ".json"; // turn name into json filename
+        // load data into memory
+        Object obj = new JSONParser().parse(new FileReader(filepath));
+        // cast as JSONObject
+        JSONObject hostile = (JSONObject) obj;
+        // recover data
+        String type = (String) hostile.get("type");
+        String name = (String) hostile.get("name");
+        String description = (String) hostile.get("description");
+        Element attribute = switch ((int) hostile.get("attribute")) {
+            case -1 -> null;
+            case 0 -> new Air();
+            case 1 -> new Earth();
+            case 2 -> new Water();
+            case 3 -> new Fire();
+            default -> {throw new UndefinedKeyException((int) hostile.get("attribute") +
+                    " is not a valid elemental attribute.");}
+        };
+        int level = (int) hostile.get("level");
+        // stats
+        int[] stats = Helpers.jsonArrayToIntArray((JSONArray) hostile.get("stats"));
+        // loot
+        JSONArray ja = (JSONArray) hostile.get("loot");
+        int i = 0;
+        Item[] loot = new Item[ja.size()];
+        for(Object o : ja){
+            loot[i++] = loadItem((String) o);
+        }
+        int forms = (int) hostile.get("forms");
+        // stats to enhance
+        String[] enhanced = null;
+        if ((JSONArray) hostile.get("enhanced") != null){ // convert to String[] if defined
+            enhanced = Helpers.jsonArrayToStringArray((JSONArray) hostile.get("enhanced"));
+        }
+        // construct hostile mob and return it
+        return switch(type){
+            case "monster" -> new Monster(name, description, attribute, level, stats, loot);
+            case "boss" -> new Boss(name, description, attribute, level, stats, loot, forms, enhanced);
+            default -> {throw new UndefinedKeyException(type + "is not a valid Action type.");}
+        };
+    }
 }
